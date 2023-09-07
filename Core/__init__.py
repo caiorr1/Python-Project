b96b79b3c25ec518a40ed97b28dd0a65a206b3fe
projetosfiap <rm99759@fiap.com.br>
2023-09-07 16:38:31 -0300

    refactor:  excluindo arquivos desnecessários

---
+++ Core/__init__.py
@@ -0,0 +1,4 @@
+from .cadastro import *
+from .login import *
+from .entrada import *
+from .menu import *
--- Core/imports.py
+++
@@ -1,4 +0,0 @@
-from Core.cadastro import *
-from Core.login import *
-from Core.imports import *
-from Core.entrada import *
